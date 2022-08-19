3bf58ae698ea7ac4688d4b27d3ad7c9de95880e9
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2022-08-19 14:53:37 -0300

    instalation react Reanimated

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { FlatList, StatusBar, ViewToken } from "react-native";
 import {
   CarImage,
   CarImageWrapper,
@@ -11,17 +12,31 @@ interface Props {
   imagesUrls: string[];
 }
 
+interface ChangeImageProps {
+  viweableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 export function ImageSlider({ imagesUrls }: Props) {
   return (
     <Container>
+      <StatusBar barStyle="dark-content" />
       <ImageIndexs>
-        <ImageIndex active />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
+        {imagesUrls.map((_, index) => (
+          <ImageIndex key={String(index)} active />
+        ))}
       </ImageIndexs>
-      <CarImageWrapper>
-        <CarImage source={{ uri: imagesUrls[0] }} resizeMode="contain" />
-      </CarImageWrapper>
+      <FlatList
+        data={imagesUrls}
+        keyExtractor={(key) => key}
+        renderItem={({ item }) => (
+          <CarImageWrapper>
+            <CarImage source={{ uri: item }} resizeMode="contain" />
+          </CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
     </Container>
   );
 }
